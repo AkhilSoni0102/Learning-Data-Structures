84c55847b6c7dd3703721fe008df3ff87af415b9
AkhilSoni0102 <soni18@iitg.ac.in>
2021-06-21 17:29:46 +0530

    -

--- Dynamic Programming AV/Unbounded Knapsack Problems/Coin Change Maximum no of ways.cpp
+++ Dynamic Programming AV/Unbounded Knapsack Problems/Coin Change Maximum no of ways.cpp
@@ -10,11 +10,11 @@ int main(){
     for(int i = 0;i < sum+1;i++)
         t[0][i] = 0;
     for(int i = 0;i < n + 1;i++)
-        t[i][0] = 0;
+        t[i][0] = 1;
     for(int i = 1;i < n+1;i++)
         for(int j = 1;j < sum + 1;j++)
             if(coin[i-1] <= j)
-                t[i][j] = coin[i-1] + t[i][j-coin[i-1]] + t[i-1][j];
+                t[i][j] = t[i][j-coin[i-1]] + t[i-1][j];
             else 
                 t[i][j] = t[i-1][j];
     cout <<t[n][sum];
